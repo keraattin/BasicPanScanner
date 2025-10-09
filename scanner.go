db3356d39e23ed0495b585bfd0f5456e36e0812e
keraattin <kerem-oruc@outlook.com>
2025-10-09 11:53:26 +0300

    version variable added

--- scanner.go
+++ scanner.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func main() {
+    // Declare a variable to hold version number
+    var version string = "0.01"
+    
     fmt.Println("PCI Scanner Starting...")
+    fmt.Println("Version:", version)
 }
